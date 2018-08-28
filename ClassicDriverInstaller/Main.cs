8acac1be97a6426105a46d478a7a401018aefbef
Daniel <790119+DanTheMan827@users.noreply.github.com>
2018-08-28 14:47:56 -0500

    First commit

---
+++ ClassicDriverInstaller/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClassicDriverInstaller
+{
+    public static class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            string appPath = AppDomain.CurrentDomain.BaseDirectory;
+            string file; // Contains name of certificate file
+            X509Store store = new X509Store(StoreName.AuthRoot, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(new X509Certificate2(X509Certificate2.CreateFromSignedFile(Path.Combine(appPath, "Nintendo_Classic.cat"))));
+            store.Close();
+            var process = Process.Start(new ProcessStartInfo()
+            {
+                FileName = "pnputil",
+                Arguments = $"-i -a \"{Path.Combine(appPath, "Nintendo_Classic.inf")}\"",
+                Verb = "runas",
+                WindowStyle = ProcessWindowStyle.Hidden
+                
+            });
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+    }
+}
